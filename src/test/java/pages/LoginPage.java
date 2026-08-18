0d3e95cf0bc9facad4670265e3c777e2ea5d3437
Swapnil Patil <swapnil.patil@bakuun.com>
2026-08-18 17:20:18 +0530

    update

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,10 +1,14 @@
 
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 
@@ -39,7 +43,9 @@ public class LoginPage {
 
  public void clickContinue()
  {
-  continueBtn.click();
+	 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	 wait.until(ExpectedConditions.elementToBeClickable(continueBtn));
+  //continueBtn.click();
  }
  
  public void clickpropfile() throws InterruptedException
